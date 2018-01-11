c740060658df61ded97b61e911fcf45fc6d29466
victor <gwli@outlook.com>
2018-01-11 13:09:21 +0800

    add docker_cmd

---
+++ docker_cmd.sh
@@ -0,0 +1,3 @@
+docker run -it --rm -p 8888:8888 -v `pwd`:/home/jovyan/work/pomegranate victorgwli/pomegranate 
+#docker run -it --rm -p 8888:8888 -v `pwd`:/home/jovyan/work/pomegranate victorgwli/pomegranate /bin/bash
+
